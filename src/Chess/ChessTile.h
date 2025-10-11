6251e851f02e3ae8890c3900a4b1c17de6d32716
IdoLazer <idolazer8@gmail.com>
2025-10-11 23:33:59 +0300

    Highlight not only borders but also tile color

--- src/Chess/ChessTile.h
+++ src/Chess/ChessTile.h
@@ -11,7 +11,19 @@ public:
 
     virtual void Render() override;
 
-    void ToggleHighlight(bool highlighted) { m_isHighlighted = highlighted; }
+    void ToggleHighlight(bool highlighted)
+    {
+        Color highlightColor{Color(0.1f, 0.1f, 0, 1)};
+        if (highlighted && !m_isHighlighted)
+        {
+            m_color = m_color + highlightColor;
+        }
+        else if (!highlighted && m_isHighlighted)
+        {
+            m_color = m_color - highlightColor;
+        }
+        m_isHighlighted = highlighted;
+    }
 
 private:
     bool m_isHighlighted = false;
